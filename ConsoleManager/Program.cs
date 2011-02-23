aeb5fdc24f35e20ce8689e4dd0cc5b7d8a6e61c9
Andrew Burgess <abombm1@gmail.com>
2011-02-23 00:27:39 -0500

    Updated the data model for the application.
    Added in a Console project for working directly with the DB while the interface is under construction.
    This project should aid in developing the needed Controllers and methods by testing out the model and
    getting a better idea of what data will need to be stored

---
+++ ConsoleManager/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Model;
+
+namespace ConsoleManager
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			
+		}
+	}
+}
